640a65e6da7257a5d0bc2d3b0e3a5a7271c061da
HermanYarenko <teyonel@icloud.com>
2022-12-14 20:17:58 +0300

    zadacha23

---
+++ DZ_ZADACHA23/Program.cs
@@ -0,0 +1,7 @@
+﻿ Console.WriteLine ("a");
+ int a = int.Parse(Console.ReadLine());
+ Console.WriteLine ("b"); 
+int b = int.Parse(Console.ReadLine());
+
+ int x = (int)Math.Pow(a, b);
+Console.WriteLine(x);
